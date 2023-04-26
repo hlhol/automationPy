f624f8de455ce4b13c7e3d94db79ee87dfd54f92
7ussain Mohamed <hlhols313@gmail.com>
2023-04-26 21:12:39 +0300

    add webdriver and open moddle page

--- login_moodle/moodle.py
+++ login_moodle/moodle.py
@@ -1,7 +1,17 @@
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 # declare your username and pass in verible:
 username = "username"
 password = "password"
+
+#driver of firefox
+driver = webdriver.Firefox()
+
+#open the website of moodle
+driver.get("https://moodle.polytechnic.bh/moodle/login/")
+
+
+
 
