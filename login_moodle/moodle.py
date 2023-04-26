9927a5e9e60cca172caca1c1ba32c327555c5a5d
7ussain Mohamed <hlhols313@gmail.com>
2023-04-26 20:56:32 +0300

    create an automation using selinum to login to moodle of polyticnic

---
+++ login_moodle/moodle.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+
+# declare your username and pass in verible:
+username = "username"
+password = "password"
+
